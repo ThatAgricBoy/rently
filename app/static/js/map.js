cdd6095d4ff391fe22380b3c2fef1de21cb839ae
ThatAgricBoy <samueljohn3999@gmail.com>
2023-09-13 16:40:55 +0100

    edited listing page

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,34 +1,51 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const searchInput = document.querySelector('.search');
+function initMap() {
+    const searchInputs = document.querySelectorAll('.search');
     const geocoder = new google.maps.Geocoder();
     const mapElement = document.getElementById('map');
 
     const mapOptions = {
-        center: desiredLocation, // Use the desiredLocation variable as the center
+        center: {lat: 0, lng: 0},
         zoom: 10,
     };
 
     const map = new google.maps.Map(mapElement, mapOptions);
 
-    searchInput.addEventListener('keypress', function(event) {
-        if (event.key === 'Enter') {
-            geocodeAddress(searchInput.value, map);
-        }
-    });
-
-    function geocodeAddress(address, map) {
-        geocoder.geocode({ 'address': address }, function(results, status) {
-            if (status === 'OK') {
-                const location = results[0].geometry.location;
-                map.setCenter(location);
-                // You can add a marker if you want
-                new google.maps.Marker({
-                    position: location,
-                    map: map,
-                });
-            } else {
-                console.error('Geocode was not successful for the following reason: ' + status);
+    searchInputs.forEach(function (searchInput) {
+        searchInput.addEventListener('keypress', function (event) {
+            if (event.key === 'Enter') {
+                const inputValue = searchInput.value;
+                console.log('Input value:', inputValue); // Log the input value
+                if(inputValue) {
+                    geocodeAddress(inputValue, map);
+                } else {
+                    console.error('No input value provided');
+                }
             }
         });
-    }
-});
+    });
+}
+
+function geocodeAddress(address, map) {
+    const geocoder = new google.maps.Geocoder();
+    console.log('Geocoding address:', address);
+    geocoder.geocode({'address': address}, function (results, status) {
+        console.log('Geocoding status:', status); // Log the status
+        if (status === 'OK') {
+            const location = results[0].geometry.location;
+            console.log('Geocoded location:', location); // Log the geocoded location
+
+            const marker = new google.maps.Marker({
+                position: location,
+                map: map,
+                title: address
+            });
+
+            map.setCenter(location);
+            map.setZoom(15);
+        } else {
+            console.error('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', initMap);
