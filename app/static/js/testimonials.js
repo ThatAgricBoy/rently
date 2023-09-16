3db773d49dbc79ffd4e1274fc07e866048bd7b85
ThatAgricBoy <samueljohn3999@gmail.com>
2023-09-16 12:20:44 +0100

    made some changes to the homepage

---
+++ app/static/js/testimonials.js
@@ -0,0 +1,21 @@
+const testimonials = document.querySelectorAll('.testimonial');
+let currentIndex = 0;
+
+function showTestimonial(index) {
+    testimonials.forEach((testimonial, i) => {
+        testimonial.style.display = i === index ? 'block' : 'none';
+    });
+}
+
+function nextTestimonial() {
+    currentIndex = (currentIndex + 1) % testimonials.length;
+    showTestimonial(currentIndex);
+}
+
+function prevTestimonial() {
+    currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
+    showTestimonial(currentIndex);
+}
+
+// Show the first testimonial initially
+showTestimonial(currentIndex);
